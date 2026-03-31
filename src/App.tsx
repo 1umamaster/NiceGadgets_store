8d4535d08c5e65902b796f4bb61275a50841fd0f
1umamaster <lukasholeg02@gmail.com>
2026-03-31 16:09:31 +0300

    Nice Gadgets store final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,70 @@
-import React from 'react';
-import './App.scss';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { useAppInit } from './hooks/useAppInit';
+import { CATEGORIES } from './modules/shared/constants/categories';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { AppLayout } from './modules/shared/components/AppLayout';
+import { HomePage } from './modules/Home/HomePage';
+import { CatalogPage } from './modules/Catalog';
+import { ProductDetailsPage } from './modules/ProductDetails';
+import { CartPage } from './modules/Cart';
+import { FavoritesPage } from './modules/Favorites';
+import { NotFoundPage } from './modules/NotFound';
+import { Loader } from './modules/shared/components/Loader';
 
-export const App: React.FC = () => {
+export const App = () => {
+  const { products, categoriesWithCounts, isLoading, hasError } = useAppInit();
+
+  if (isLoading) {
+    return (
+      <div className="app-initial-state">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <div className="app-initial-state">
+        <h1 className="error-title">Something went wrong</h1>
+        <button
+          type="button"
+          className="reload-button"
+          onClick={() => window.location.reload()}
+        >
+          Reload page
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AppLayout categories={categoriesWithCounts} products={products} />
+          }
+        >
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+
+          {CATEGORIES.map(category => (
+            <Route
+              key={category.id}
+              path={category.path}
+              element={<CatalogPage />}
+            />
+          ))}
+
+          <Route path="cart" element={<CartPage />} />
+          <Route path="favorites" element={<FavoritesPage />} />
+          <Route path="product/:productId" element={<ProductDetailsPage />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 };
